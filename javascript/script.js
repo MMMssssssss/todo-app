c2dc56985739c81d66a80d61be3df197c9ada86d
marushka <matthews0404@icloud.com>
2026-02-25 10:02:06 +0200

    Improved styling and separated HTML, CSS, and JS files

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,21 @@
+const inputTask = document.getElementById("inputTask");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
 
+addButton.addEventListener("click", addTask);
+
+function addTask() {
+    const inputValue = inputTask.value.trim();
+
+    if (inputValue !== "") {
+        const newTask = document.createElement("p");
+
+        newTask.textContent = inputValue;
+
+        taskList.appendChild(newTask);
+
+        inputTask.value = "";
+    } else {
+        alert("Please enter a task.");
+    }
+}
