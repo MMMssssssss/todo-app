09d872a9826533df6ecfcf49d494fc1c928a0448
marushka <matthews0404@icloud.com>
2026-04-22 11:29:37 +0200

    Updated script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 const inputTask = document.getElementById('inputTask');
 const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
-=======
-const inputTask = document.getElementById("inputTask");
-const addButton = document.getElementById("addButton");
-const taskList = document.getElementById("taskList");
->>>>>>> c2dc56985739c81d66a80d61be3df197c9ada86d
 
 addButton.addEventListener("click", addTask);
 
@@ -14,7 +8,7 @@ function addTask() {
     const inputValue = inputTask.value.trim();
 
     if (inputValue !== "") {
-<<<<<<< HEAD
+
         const newTask = document.createElement("li");
 
         newTask.textContent = inputValue;
@@ -25,16 +19,3 @@ function addTask() {
     }
 
 }
-=======
-        const newTask = document.createElement("p");
-
-        newTask.textContent = inputValue;
-
-        taskList.appendChild(newTask);
-
-        inputTask.value = "";
-    } else {
-        alert("Please enter a task.");
-    }
-}
->>>>>>> c2dc56985739c81d66a80d61be3df197c9ada86d
